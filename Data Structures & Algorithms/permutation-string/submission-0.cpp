208c15e9a328673a3befcba127e46791754af147
Abhiram Chandra <abhiram.chandra0318@gmail.com>
2026-06-04 15:48:47 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        sort(s1.begin(),s1.end());
+        int size=s1.size();
+        int l=0;
+        int r=size-1;
+        while(r<s2.size()){
+            string temp=s2.substr(l,size);
+            sort(temp.begin(),temp.end());
+            if(temp==s1) return true;
+            l++;
+            r++;
+        }
+        return false;
+    }
+};
