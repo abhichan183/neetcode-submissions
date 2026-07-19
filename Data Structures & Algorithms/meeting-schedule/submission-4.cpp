213fbe207a590ec73dc5ae4642571630090afe65
Abhiram Chandra <abhiram.chandra0318@gmail.com>
2026-07-19 17:55:43 +0530

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b) {
+    return a.start < b.start;
+});
+    int n=intervals.size();
+    Interval curr=intervals[0];
+    for(int i=1;i<n;i++){
+        if(curr.end>intervals[i].start) return false;
+        curr=intervals[i];
+    }
+    return true;
+    }
+};
